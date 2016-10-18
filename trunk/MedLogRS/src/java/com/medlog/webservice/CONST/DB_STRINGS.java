65907f739b058dcb7ef8910e85fa48460305006b
tue65786 <tue65786@temple.edu>
2016-10-18 17:33:33 -0400

    Initial package definitions and library imports.

---
+++ trunk/MedLogRS/src/java/com/medlog/webservice/CONST/DB_STRINGS.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package com.medlog.webservice.CONST;
+
+/**
+ *
+ * @author (c)2016 Dan Kauffman
+ */
+public class DB_STRINGS {
+/**
+ * Do not allow instantiation 
+ * 
+ */
+private DB_STRINGS(){
+   
+}
+}
