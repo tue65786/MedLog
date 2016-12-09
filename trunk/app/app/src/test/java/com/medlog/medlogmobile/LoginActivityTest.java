b5466cd4f4f25682c909ec5cc94380178871e430
tue65786 <diploria1>
2016-12-09 07:21:08 -0500

    Move documents

---
+++ trunk/app/app/src/test/java/com/medlog/medlogmobile/LoginActivityTest.java
@@ -0,0 +1,48 @@
+package com.medlog.medlogmobile;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by westy on 11/30/2016.
+ */
+public class LoginActivityTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void createIntent() throws Exception {
+
+    }
+
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+    @Test
+    public void onCreateLoader() throws Exception {
+
+    }
+
+    @Test
+    public void onLoadFinished() throws Exception {
+
+    }
+
+    @Test
+    public void onLoaderReset() throws Exception {
+
+    }
+
+}
